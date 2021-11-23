0b182ca879c4d90e0b120c53f5e53c26872de7a7
Daniel <danielalves.ccomp@gmail.com>
2021-11-23 09:19:28 -0300

    Checkmail

--- Packages/main.go
+++ Packages/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"module/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Writing file main")
 	auxiliar.Write()
+
+	err := checkmail.ValidateFormat("danielalves.ccomp")
+
+	fmt.Println(err)
 }
